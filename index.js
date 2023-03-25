3408c472f7a8d3637456fb2d9c6dab00328a2772
gich2009 <waneneamos@gmail.com>
2023-03-26 00:03:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+  const paragraphInBody = document.querySelector("p");
+
+  paragraphInBody.textContent = `This is really cool!`;
+
+})
